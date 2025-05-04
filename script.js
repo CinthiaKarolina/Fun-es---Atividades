e3308ba4cc59e2f90d4b67e330d93b9418946b92
Cinthia Karolina <cinthia.k2008@gmail.com>
2025-05-04 14:36:15 -0300

    Segundo exemplo

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/*
+function mostrarAlerta () {
+    let nome = "Márcio"
+    alert("Seja bem-vindo(a), " +nome)
+}
+mostrarAlerta ()
+*/
+
+
+function somar (valorA, valorB) {
+    alert(valorA + valorB)
+}
+somar(20, 12)
