7abfc6e1bbfa2a033b7bfa9dad01db9f95f0b0b8
Cinthia Karolina <cinthia.k2008@gmail.com>
2025-05-04 14:44:59 -0300

    Terceiro exemplo

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ function mostrarAlerta () {
 mostrarAlerta ()
 */
 
-
+/*
 function somar (valorA, valorB) {
     alert(valorA + valorB)
 }
 somar(20, 12)
+*/
+
+function mensagemBoasVindas (nome, cargo) {
+    return `Seja bem vindo(a) ${cargo} ${nome}`
+}
+
+let nome = prompt("Digite o nome do funcionário: ")
+let cargo = prompt("Digite o cargo do funcionário: ")
+
+alert(mensagemBoasVindas(nome, cargo))
+console.log(mensagemBoasVindas(nome, cargo))
